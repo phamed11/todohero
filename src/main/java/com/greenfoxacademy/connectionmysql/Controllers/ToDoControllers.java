77802714c707171624b055a5d08e605a191281c3
Peter Antal <wrkpeter@gmail.com>
2018-09-27 09:51:11 +0200

    sdfsdf

--- src/main/java/com/greenfoxacademy/connectionmysql/Controllers/ToDoControllers.java
+++ src/main/java/com/greenfoxacademy/connectionmysql/Controllers/ToDoControllers.java
@@ -2,7 +2,6 @@ package com.greenfoxacademy.connectionmysql.Controllers;
 
 import com.greenfoxacademy.connectionmysql.Models.ToDo;
 import com.greenfoxacademy.connectionmysql.Respositories.ToDoRespository;
-import com.sun.tools.javac.comp.Todo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
